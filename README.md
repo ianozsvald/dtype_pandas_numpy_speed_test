a2baf60028f29121bedaeac62810116571268e93
Ian Ozsvald <ian@morconsulting.com>
2020-06-12 19:23:30 +0100

    first

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # dtype_pandas_numpy_speed_test
 Speed test on Pandas or NumPy 
+
+# Timing
+
+Try `$ python speed_test_combined.py` and post `timings.png` as a bug with some notes on CPU if possible.
+
+This machine: i7:
+```
+$ sudo i7z
+i7z DEBUG: i7z version: svn-r93-(27-MAY-2013)
+i7z DEBUG: Found Intel Processor
+i7z DEBUG:    Stepping 3
+i7z DEBUG:    Model e
+i7z DEBUG:    Family 6
+i7z DEBUG:    Processor Type 0
+i7z DEBUG:    Extended Model 5
+i7z DEBUG: msr = Model Specific Register
+i7z DEBUG: Unknown processor, not exactly based on Nehalem, Sandy bridge or Ivy Bridge
+i7z DEBUG: msr device files DO NOT exist, trying out a makedev script
+i7z DEBUG: modprobbing for msr
+
+```
+
+[timings.png`](timings)
+
+# Setup
+
+```
+conda create -n dtype_pandas_numpy_speed_test python=3.8 pandas jupyter matplotlib watermark ipython_memory_usage memory_profiler
+```
---
+++ speed_test_combined.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import time
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#import ipython_memory_usage
+#get_ipython().run_line_magic('ipython_memory_usage_start', '')
+
+
+# In[2]:
+
+
+#get_ipython().run_line_magic('load_ext', 'watermark')
+#get_ipython().run_line_magic('watermark', '-i -v -m -p pandas,numpy,matplotlib -g -b')
+
+
+# In[3]:
+
+
+NROWS = 10_000_000
+NBR_LOOPS = 100
+MATH_FN = 'mean'
+
+dtypes = ['int64', 'int32', 'int16', 'int8', 'uint8', 'float128', 'float64', 'float32', 'float16']
+cols = {}
+for dtyp in dtypes:
+    # makes random data using normal distribution, then cast
+    # to various dtypes (which will involve rounding)
+    arr = np.random.normal(size=NROWS).astype(dtyp)
+    cols[dtyp] = arr
+df_data = pd.DataFrame(cols)
+df_data.head(5)
+
+
+# In[4]:
+
+
+df_data.info()
+
+
+# In[5]:
+
+
+cols = []
+for col in df_data.columns:
+    print(f"Working on {col}")
+    arr = df_data[col]
+    for fn_idx in range(2):
+        if fn_idx == 0:
+            fn_name = "ser"
+            fn = getattr(arr, MATH_FN)
+        else:
+            fn = getattr(getattr(arr, 'values'), MATH_FN)
+            fn_name = "ser.values"
+        for n in range(NBR_LOOPS):
+            t1 = time.time()
+            #arr.mean()
+            fn()
+            #timings[n] = time.time() - t1
+            delta = time.time() - t1
+            cols.append({'fn_name': fn_name, 'col': col, 't': delta})
+df_results = pd.DataFrame(cols)
+df_results.sample(5)
+
+
+# In[ ]:
+
+
+df_results.info()
+
+
+# In[ ]:
+
+
+gpby = df_results.groupby(['fn_name', 'col'])
+means = gpby.mean().unstack(0)
+means = means.loc[dtypes]
+means
+
+
+# In[ ]:
+
+
+stds = gpby.std().unstack(0)
+stds = stds.loc[dtypes]
+se = stds / np.sqrt(NBR_LOOPS)
+se_95pc = se * 1.96
+se_95pc
+
+
+# In[ ]:
+
+
+fig, ax = plt.subplots(figsize=(8, 6))
+means.plot(kind='bar', ax=ax, yerr=se_95pc)
+
+title = f"Fn '{MATH_FN}' execution time on {NROWS:,} rows of normal rnd over {NBR_LOOPS:,} loops"
+title += f"\nBlack bars are the 95% Confidence Interval"
+title += "\nUsing Pandas Series and Series.values"
+ax.set_title(title);
+ax.set_ylabel('Seconds (smaller is better)');
+
+locs, labels=plt.yticks()
+y_ticks = []
+new_yticks=[f"{d}s" for d in locs]
+plt.yticks(locs,new_yticks); #, rotation=45, horizontalalignment='right')
+ax.set_xlabel('dtype');
+
+ax.get_figure().savefig('timings.png')
+#plt.show()
+
+
+# In[ ]:
+
+
+
+
