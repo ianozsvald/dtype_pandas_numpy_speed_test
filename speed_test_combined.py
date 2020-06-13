37134d111cb36203fe7ab6209bb315be234ef1a7
Ian Ozsvald <ian@morconsulting.com>
2020-06-13 21:59:30 +0100

    update

--- speed_test_combined.py
+++ speed_test_combined.py
@@ -25,57 +25,65 @@ print(f"NumPy: {np.__version__}")
 
 # In[3]:
 
-
-#NROWS = 100_000_000
 NROWS = 10_000_000
 NBR_LOOPS = 100
+
+#NROWS = 1_000_000
+#NBR_LOOPS = 3 
+#NROWS = 100_000_000
+#NBR_LOOPS = 3
 MATH_FN = 'mean'
 #MATH_FN = 'std'
 #MATH_FN = 'max'
 timings_filename = "timings.png"
 results_filename = "timings.pickle"
 
-dtypes = ['int64', 'int32', 'int16', 'int8', 'uint8', 'longdouble', 'float64', 'float32', 'float16']
-#dtypes = ['int64', 'int32', 'int16', 'int8', 'uint8', 'float128', 'float64', 'float32', 'float16']
-cols = {}
-for dtyp in dtypes:
+# 'longdouble' is 16 (or so) bytes, the timings seem to be quite varied, I'm not sure measuring
+# it is useful - aka float128 and others
+dtypes = ['int64', 'int32', 'int16', 'int8', 'uint8', 'float64', 'float32', 'float16']
+#cols = {}
+#for dtyp in dtypes:
     # makes random data using normal distribution, then cast
     # to various dtypes (which will involve rounding)
-    arr = np.random.normal(size=NROWS).astype(dtyp)
-    cols[dtyp] = arr
-df_data = pd.DataFrame(cols)
-df_data.head(5)
+#    arr = np.random.normal(size=NROWS).astype(dtyp)
+#    cols[dtyp] = arr
+#df_data = pd.DataFrame(cols)
+#df_data.head(5)
 
 
 # In[4]:
 
 
-df_data.info()
+#df_data.info()
 
 
 # In[5]:
 
+bytes_used = {}
 
-cols = []
-for col in df_data.columns:
-    print(f"Working on {col}")
-    arr = df_data[col]
-    for fn_idx in range(2):
-        if fn_idx == 0:
-            fn_name = "ser"
-            fn = getattr(arr, MATH_FN)
-        else:
-            fn = getattr(getattr(arr, 'values'), MATH_FN)
-            fn_name = "ser.values"
-        for n in range(NBR_LOOPS):
-            t1 = time.time()
-            #arr.mean()
-            fn()
-            #timings[n] = time.time() - t1
-            delta = time.time() - t1
-            cols.append({'fn_name': fn_name, 'col': col, 't': delta})
-df_results = pd.DataFrame(cols)
-df_results.sample(5)
+if 'df_results' not in dir():
+    cols = []
+    #for col in df_data.columns:
+    for col in dtypes:
+        #arr = df_data[col]
+        arr = pd.Series(np.random.normal(size=NROWS, loc=1).astype(col))
+        print(f"Working on {col}, values start: {arr.values[:3]}")
+        for fn_idx in range(2):
+            if fn_idx == 0:
+                fn_name = "ser"
+                fn = getattr(arr, MATH_FN)
+            else:
+                fn = getattr(getattr(arr, 'values'), MATH_FN)
+                fn_name = "ser.values"
+            for n in range(NBR_LOOPS):
+                t1 = time.time()
+                fn()
+                #timings[n] = time.time() - t1
+                delta = time.time() - t1
+                bytes_used[col] = arr.memory_usage()
+                cols.append({'fn_name': fn_name, 'col': col, 't': delta})
+    df_results = pd.DataFrame(cols)
+    df_results.sample(5)
 
 
 # In[ ]:
@@ -84,17 +92,11 @@ df_results.sample(5)
 df_results.info()
 
 
-# In[ ]:
-
-
 gpby = df_results.groupby(['fn_name', 'col'])
-central_measure = gpby.median().unstack(0)
+#central_measure = gpby.median().unstack(0)
+central_measure = gpby.mean().unstack(0)
 central_measure = central_measure.loc[dtypes]
 central_measure
-
-
-# In[ ]:
-
 
 stds = gpby.std().unstack(0)
 stds = stds.loc[dtypes]
@@ -102,35 +104,32 @@ se = stds / np.sqrt(NBR_LOOPS)
 se_95pc = se * 1.96
 se_95pc
 
-
-# In[ ]:
-
-
 fig, ax = plt.subplots(figsize=(8, 6))
 central_measure.plot(kind='bar', ax=ax, yerr=se_95pc)
 
 title = f"Fn '{MATH_FN}' execution time on {NROWS:,} rows of normal rnd over {NBR_LOOPS:,} loops"
-title += f"\nBlack bars are the 95% Confidence Interval. Central measure is: median"
+title += f"\nBlack bars are the 95% Confidence Interval. Central measure is: mean"
 title += "\nUsing Pandas Series and Series.values"
 ax.set_title(title);
 ax.set_ylabel('Seconds (smaller is better)');
 
-locs, labels=plt.yticks()
+locs, labels = plt.yticks()
 y_ticks = []
-new_yticks=[f"{d}s" for d in locs]
+new_yticks=[f"{d:0.3f}s" for d in locs]
 plt.yticks(locs,new_yticks); #, rotation=45, horizontalalignment='right')
+
+locs, labels = plt.xticks()
+new_xticks = [f"{l.get_text()} (≈{int(bytes_used[l.get_text()]/100_000):,} MB)" for l in labels]
+#{bytes_used[s]}
+plt.xticks(locs, new_xticks); #, rotation=45, horizontalalignment='right')
 ax.set_xlabel('dtype');
+
 ax.get_figure().tight_layout()
 ax.grid()
 
 ax.get_figure().savefig(timings_filename)
 #plt.show()
 
-with open(results_filename, "wb") as f:
-    pickle.dump(df_results, f)
-
-# In[ ]:
-
-
-
-
+if False:
+    with open(results_filename, "wb") as f:
+        pickle.dump(df_results, f)
